72554432a3f7c678d84253ae437bfefb0e619c3a
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-12-12 15:14:33 -0700

    Set up tortoise-orm to interface with postgres database

--- project/app/main.py
+++ project/app/main.py
@@ -1,11 +1,23 @@
+import os
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
 app = FastAPI()
+
+
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {'ping': 'pong!',
             'environment': settings.environment,
             'testing': settings.testing}
+
+register_tortoise(
+    app,
+    db_url=os.environ.get('DATABASE_URL'),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
